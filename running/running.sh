f74319eaad3d129f0bece781e8d20909d4954540
Kebson23 <147546216+Kebson23@users.noreply.github.com>
2025-12-15 10:41:47 +0100

    Finalize v1.0 ansible playground

--- running/running.sh
+++ running/running.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-exec sudo /usr/bin/shellinaboxd --no-beep --user=${shellinabox_USER} --group=${shellinabox_USER} --port=${shellinabox_PORT} --css=/etc/shellinabox/options-enabled/00_WhiteOnBlack.css --disable-ssl
+exec sudo /usr/bin/shellinaboxd --no-beep --user=${shellinabox_USER} --group=${shellinabox_USER} --port=${shellinabox_PORT} -q \
+ -c /var/lib/shellinabox \
+ --disable-ssl \
+ --user-css "Normal:+/etc/shellinabox/options-enabled/00_White On Black.css;Colors:+/etc/shellinabox/options-enabled/01+Color Terminal.css"
